c350bd4b93d35c3270f4863f40f56b6eae5b6f16
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-09-23 17:09:16 +0200

    [LAVA-1] Add connection to Database (#1)
    * Remove secrets, add config module
    
    * Fix winston
    
    * Fix tsconfig
    
    * Test of DB connection

---
+++ config/development.ts
@@ -0,0 +1,13 @@
+export default {
+  database: {
+    engine: "mysql",
+    host: "127.0.0.1",
+    maxConnections: 25,
+    maxIdleTime: 30000,
+    minConnections: 0,
+    name: "",
+    password: "",
+    port: "",
+    user: ""
+  }
+};
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import database from "../database";
 
 /**
  * GET /api
  * List of API examples.
  */
 export let getApi = (req: Request, res: Response) => {
-  return res.json({
-    hello: "world"
-  });
+  database.query("SELECT * FROM game_heroes", { type: database.QueryTypes.SELECT})
+    .then(users => {
+      return res.json({
+        hello: users
+      });
+    });
+
 };
