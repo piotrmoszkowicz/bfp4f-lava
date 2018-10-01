ab44dcb79692683acb68d261d791a01bb6253990
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-01 21:54:09 +0200

    Add getBoostersJson

--- src/controllers/get/index.ts
+++ src/controllers/get/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { getApparelJson } from "./getApparelJson";
+import { getBoostersJson } from "./getBoostersJson";
 import { getWalletBalance } from "./getWalletBalance";
 import { getWeaponsJson } from "./getWeaponsJson";
 
 const router: Router = Router();
 
 router.get("/getApparelJson", getApparelJson);
+router.get("/getBoostersJson", getBoostersJson);
 router.get("/getWalletBalance", getWalletBalance);
 router.get("/getWeaponsJson", getWeaponsJson);
 
