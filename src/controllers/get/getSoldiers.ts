47b6ddaed98e82ea3d36fb16fb4154f513343a59
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-01 23:21:35 +0200

    Add getSoldiers

---
+++ src/controllers/get/getSoldiers.ts
@@ -0,0 +1,39 @@
+import { Response } from "express";
+import { RequestBFP4F } from "ExpressOverride";
+
+import SoldierService from "../../services/soldierService";
+
+import Logger from "../../util/logger";
+
+export const getSoldiers = async (
+  req: RequestBFP4F,
+  res: Response
+): Promise<Response> => {
+  try {
+    const soldiers = await SoldierService.getSoldiersBySessionId(req.sessionId);
+    return res.json({
+      result: "success",
+      status: "success",
+      data: {
+        personas: soldiers.map(soldier => ({
+          id: soldier.id,
+          name: soldier.soldierName,
+          kit: soldier.kit,
+          xp: 0, // TODO: Add XP
+          xpForNextLevel: 0, // TODO: Add XP for nextLevel
+          lastAuthenticated: 0, // TODO: Add lastAuthed
+          mugShot: "", // TODO: Add mugshots
+          isMaxLevel: !!(soldier.level === 30),
+          level: soldier.level,
+          levelUpProgression: 0, // TODO: Add level progression
+          levelDescription: "Asdf" // TODO: Add level descriptions
+        }))
+      }
+    });
+  } catch (err) {
+    Logger.error("Error in /getSoldiers", err);
+    return res.json({
+      status: "error"
+    });
+  }
+};
