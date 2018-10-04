91576c1c538ada395cb82a67563a79055853904e
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-04 23:37:32 +0200

    Add PurchaseTrainingPoints type

---
+++ src/types/PurchaseTrainingPoints.d.ts
@@ -0,0 +1,11 @@
+type PurchaseTrainingPoints = {
+  result: string;
+  status: string;
+  data: {
+    credits: number;
+    funds: number;
+    purchasedPoints: number;
+    trainingPoints: number;
+    offers: any;
+  }
+};
