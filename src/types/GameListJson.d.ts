3f0e47c1d25596264fa99cfbc7d519546b71bb60
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-21 02:45:39 +0200

    Add types for server stuff

---
+++ src/types/GameListJson.d.ts
@@ -0,0 +1,35 @@
+type GameListJson = {
+  gameId: string;
+  persistentId: string;
+  name: string;
+  mapList: string[];
+  mapIndex: string;
+  roundsPerMap: string;
+  currentRounds: string;
+  gameMode: string;
+  ranked: boolean;
+  hasPassword: boolean;
+  players: string;
+  capacity: string;
+  pingSite: ServerRegion;
+  ip: string;
+  levelAvg: number;
+  levelSdv: number;
+  online: boolean;
+  isVIP: boolean;
+  bookmarked: boolean;
+  currentMap: string;
+  currentGameMode: string;
+  nextMap: string;
+  nextGameMode: string;
+}
+
+type GameListJsonResponse = {
+  result: string;
+  status: string;
+  data: {
+    [key: string]: GameListJson
+  };
+};
+
+export { GameListJson, GameListJsonResponse };
---
+++ src/types/ServerRegion.d.ts
@@ -0,0 +1,7 @@
+enum ServerRegion {
+  Asia = "nrt",
+  Australia = "syd",
+  Europe = "gva",
+  UsWest = "sjc",
+  UsEast = "iad"
+}
