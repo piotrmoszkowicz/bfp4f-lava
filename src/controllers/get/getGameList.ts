0cfab36cf16415673d3e7e3029f4f9bbefea4dfa
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-21 02:46:11 +0200

    Add getGameList route

---
+++ src/controllers/get/getGameList.ts
@@ -0,0 +1,27 @@
+import { Response } from "express";
+import { RequestBFP4F } from "ExpressOverride";
+
+import ServerService from "../../services/serverService";
+
+import Logger from "../../util/logger";
+
+export const getGameList = async (
+  req: RequestBFP4F,
+  res: Response
+): Promise<Response> => {
+  try {
+    const serverlist = await ServerService.getAllServers();
+    Logger.info(serverlist);
+    return res.json({
+      result: "success",
+      status: "success",
+      data: ServerService.parseServersForResponse(serverlist)
+    });
+  } catch (err) {
+    Logger.error("Error in /getGameList", err);
+    return res.json({
+      result: "error",
+      status: "error"
+    });
+  }
+};
