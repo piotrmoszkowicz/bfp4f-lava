85279c287295867195309cb04a76c685f9acb719
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-01-08 11:50:16 +0100

    Add sentry to logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,11 +1,17 @@
 import winston from "winston";
+import Sentry from "winston-sentry-raven-transport";
 
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === "production" ? "error" : "debug"
     }),
-    new winston.transports.File({ filename: "debug.log", level: "debug" })
+    new winston.transports.File({ filename: "debug.log", level: "debug" }),
+    new Sentry({
+      level: "warn",
+      dns: "https://20ceb3e80a8e493285dd44def5376748@sentry.io/1366619",
+      patchGlobal: true
+    })
   ]
 });
 
