d367c312c080accbd66c843d9767ebae9c6c5db2
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-29 01:29:55 +0100

    [LAVA-40] Move needed stuff to CDN (#54)
    * Remove static files
    
    * Remove CSS controller, remove static routes
    
    * Update index.html file
    
    * URL changes for html controller

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,12 @@ import express, { NextFunction, Response } from "express";
 import expressSession from "express-session";
 import expressValidator from "express-validator";
 import { RequestBFP4F } from "ExpressOverride";
-import path from "path";
 
 import database from "./database";
 import soldierService from "./services/soldierService";
 import Logger from "./util/logger";
 
 // Controllers (route handlers)
-import { CssRouter } from "./controllers/css";
 import { GameRouter } from "./controllers/game";
 import { HtmlRouter } from "./controllers/html";
 
@@ -35,23 +33,6 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-
-app.use("/css", CssRouter, cors(corsOptions));
-app.use(
-  "/js",
-  express.static(path.join(__dirname, "../", "public", "js")),
-  cors(corsOptions)
-);
-app.use(
-  "/static",
-  express.static(path.join(__dirname, "../", "public", "static")),
-  cors(corsOptions)
-);
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "../", "public", "uploads")),
-  cors(corsOptions)
-);
 
 app.use((req: RequestBFP4F, res: Response, next: NextFunction) => {
   if (!req.cookies || !req.cookies.magma) {
