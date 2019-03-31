cf38bc4334efe43139ee3effec118101450e0637
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-31 21:50:04 +0200

    Update logger to debug when we only want to and give much better response

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,22 +1,35 @@
+import config from "config";
 import winston from "winston";
 import Sentry from "winston-sentry-raven-transport";
 
+
+const { combine, timestamp, prettyPrint } = winston.format;
+
+const transports = [
+  new winston.transports.Console({
+    level: config.get("lava.debug") ? "debug" : "warn"
+  }),
+  new Sentry({
+    level: "warn",
+    dsn: "https://20ceb3e80a8e493285dd44def5376748@sentry.io/1366619",
+    patchGlobal: true
+  })
+];
+
+if (config.get("lava.debug")) {
+  transports.push(new winston.transports.File({ filename: "debug.log", level: "debug" }));
+}
+
 const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      level: process.env.NODE_ENV === "production" ? "error" : "debug"
-    }),
-    new winston.transports.File({ filename: "debug.log", level: "debug" }),
-    new Sentry({
-      level: "warn",
-      dsn: "https://20ceb3e80a8e493285dd44def5376748@sentry.io/1366619",
-      patchGlobal: true
-    })
-  ]
+  format: combine(
+    timestamp(),
+    prettyPrint()
+  ),
+  transports
 });
 
-if (process.env.NODE_ENV !== "production") {
-  logger.debug("Logging initialized at debug level");
+if (config.get("lava.debug")) {
+  logger.log("debug", "Logging initialized at debug level" );
 }
 
 export default logger;
