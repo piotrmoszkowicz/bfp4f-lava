ea97d6a98b6aa88bbb077816dca7170e3609add2
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-07-24 23:11:20 +0200

    Make logger more reasonable, add DSN to the config files

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,20 +4,23 @@ import Sentry from "winston-sentry-raven-transport";
 
 const { combine, timestamp, prettyPrint } = winston.format;
 
-const transports = [
+const transports: any = [
   new winston.transports.Console({
     level: config.get("lava.debug") ? "debug" : "warn"
-  }),
-  new Sentry({
-    level: "warn",
-    dsn: "https://20ceb3e80a8e493285dd44def5376748@sentry.io/1366619",
-    patchGlobal: true
   })
 ];
 
 if (config.get("lava.debug")) {
   transports.push(
     new winston.transports.File({ filename: "debug.log", level: "debug" })
+  );
+} else {
+  transports.push(
+    new Sentry({
+      level: "warn",
+      dsn: config.get("lava.dsn"),
+      patchGlobal: true
+    })
   );
 }
 
